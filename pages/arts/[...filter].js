4d1bbdff0005552a05c7a22c13c74c8340c53cb0
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-03-22 18:42:15 +0530

    implimented search result UI

--- pages/arts/[...filter].js
+++ pages/arts/[...filter].js
@@ -1,7 +1,37 @@
+import { useRouter } from 'next/router'
+import React, { useEffect, useState } from 'react'
+import { GetFilteredArts } from '../../app-demo-data'
+import ArtList from '../../components/art-list'
+import { ThemeProvider } from './../../app-context'
+
 export default function ArtFilteredPage() {
-  return (
-    <div>
-      <h1>Art With Filter Page!</h1>
-    </div>
-  )
+	const router = useRouter()
+	const pageSize = 6
+	const [artRecordCount, setArtRecordCount] = useState([])
+	const [artRecords, setArtRecords] = useState([])
+
+	useEffect(() => {
+		if (router.asPath !== router.route) {
+			const year = router.query.filter[1]
+			const month = router.query.filter[0]
+			const pageFilter = { year, month }
+			const records = GetFilteredArts(pageFilter)
+			if (records != undefined) {
+				setArtRecords(records)
+			}
+		}
+	}, [router])
+
+	return (
+		<ThemeProvider>
+			<div>
+				<p>&nbsp;</p>
+				<div className='container is-max-fluid is-centered'>
+					<div className='columns is-mobile is-multiline'>
+						<ArtList data={artRecords} />
+					</div>
+				</div>
+			</div>
+		</ThemeProvider>
+	)
 }
