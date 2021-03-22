ee8a454e512e6c67c289899afc734f77b2470fa8
Nhilesh Baua <nhilesh.baua@gmail.com>
2021-03-22 09:11:29 +0530

    fixed url tempering

--- pages/arts/[id].js
+++ pages/arts/[id].js
@@ -12,16 +12,13 @@ export default function ArtDetailsPage(props) {
 	useEffect(() => {
 		if (router.asPath !== router.route) {
 			const artItem = GetArtById(router.query.id)[0]
-			console.log(artItem)
-			setArtRecord(artItem)
+			if (artItem != undefined) {
+				setArtRecord(artItem)
+			}
 		}
 	}, [router])
 
-	// return (
-	// 	<div className='column is-half mt-6'>
-	// 		<h1 className='column is-half mt-6'>{title.length}</h1>
-	// 	</div>
-	// )
+	function handleTempering() {}
 
 	return (
 		<div className='columns is-mobile is-centered'>
@@ -37,31 +34,35 @@ export default function ArtDetailsPage(props) {
 						<Image className='image is-4by3' src={image === undefined ? '/images/loved.svg' : image} alt={title} width={960} height={600} />
 					</div>
 					<footer className='card-footer'>
-						<div className='card-footer-item column is-narrow'>
-							<div className='mr-1 mb-1 has-text-centered is-unselectable'>
-								<Image src='/images/views.svg' alt={id} width={24} height={24} />
-								<h1 className='is-size-7 is-family-code '>{views}</h1>
-							</div>
-						</div>
-						<div className='card-footer-item column is-narrow'>
-							<div className='mr-1 mb-1 has-text-centered is-unselectable'>
-								<Image src='/images/likes.svg' alt={id} width={24} height={24} />
-								<h1 className='is-size-7 is-family-code'>{likes}</h1>
-							</div>
-						</div>
-						<div className='card-footer-item column is-narrow'>
-							<div className='mr-1 mb-1  is-unselectable'>
-								<h1 className='is-size-6 is-family-sans-serif'>Shot At: </h1>
-								<h1 className='is-size-6 is-family-sans-serif'>{location}</h1>
-							</div>
-						</div>
-						<div className='card-footer-item column is-narrow'>
-							<div className='mr-1 mb-1  is-unselectable'>
-								<h1 className='is-size-6 is-family-sans-serif'>Listed Since: </h1>
-								<h1 className='is-size-6 is-family-sans-serif'>{showcaseDate}</h1>
-							</div>
-						</div>
-						<div className='card-footer-item is-size-6 is-family-sans-serif	is-unselectable'>{description}</div>
+						{id !== undefined && (
+							<>
+								<div className='card-footer-item column is-narrow'>
+									<div className='mr-1 mb-1 has-text-centered is-unselectable'>
+										<Image src='/images/views.svg' alt={id} width={24} height={24} />
+										<h1 className='is-size-7 is-family-code '>{views}</h1>
+									</div>
+								</div>
+								<div className='card-footer-item column is-narrow'>
+									<div className='mr-1 mb-1 has-text-centered is-unselectable'>
+										<Image src='/images/likes.svg' alt={id} width={24} height={24} />
+										<h1 className='is-size-7 is-family-code'>{likes}</h1>
+									</div>
+								</div>
+								<div className='card-footer-item column is-narrow'>
+									<div className='mr-1 mb-1  is-unselectable'>
+										<h1 className='is-size-6 is-family-sans-serif'>Shot At: </h1>
+										<h1 className='is-size-6 is-family-sans-serif'>{location}</h1>
+									</div>
+								</div>
+								<div className='card-footer-item column is-narrow'>
+									<div className='mr-1 mb-1  is-unselectable'>
+										<h1 className='is-size-6 is-family-sans-serif'>Listed Since: </h1>
+										<h1 className='is-size-6 is-family-sans-serif'>{showcaseDate}</h1>
+									</div>
+								</div>
+							</>
+						)}
+						<div className='card-footer-item is-size-6 is-family-sans-serif	is-unselectable'>{description !== undefined ? description : 'Sorry folks! did not found your art.'}</div>
 					</footer>
 				</div>
 			</div>
